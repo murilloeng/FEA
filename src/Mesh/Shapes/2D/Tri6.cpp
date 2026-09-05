c2bd9be27eb181e0f20b79e8e4e2dd51dcd036da
Murillo Santana <mvbentosantana@gmail.com>
2026-09-05 19:58:02 +0200

    update tri6 and tri10 shape functions

--- src/Mesh/Shapes/2D/Tri6.cpp
+++ src/Mesh/Shapes/2D/Tri6.cpp
@@ -1,16 +1,6 @@
 //FEA
 #include "FEA/inc/Mesh/Shapes/2D/Tri6.hpp"
 
-//static
-static const double A[] = {
-	+0, +0, +0, +0, +2, +0,
-	+1, +1, +0, -2, +2, -2,
-	+1, +0, +1, -2, +2, -2,
-	+1, +1, +0, -2, +0, +0,
-	+2, +0, +0, -2, +2, -2,
-	+1, +0, +1, +0, +0, -2
-};
-
 namespace fea
 {
 	namespace mesh
@@ -42,27 +32,32 @@ namespace fea
 				const double r = p[0];
 				const double s = p[1];
 				//shape
-				
+				N[1] = r * (1 + r) / 2;
+				N[2] = s * (1 + s) / 2;
+				N[3] = -(1 + r) * (r + s);
+				N[4] = +(1 + r) * (1 + s);
+				N[5] = -(1 + s) * (r + s);
+				N[0] = +(r + s) * (1 + r + s) / 2;
 			}
 			void Tri6::gradient(double* B, const double* p) const
 			{
 				//data
 				const double r = p[0];
 				const double s = p[1];
-				const double dv[] = {
-					0, 1, 0, 2 * r, s, 0,
-					0, 0, 1, 0, r, 2 * s
-				};
-				//gradient
-				for(uint32_t i = 0; i < 6; i++)
-				{
-					B[i + 0] = B[i + 6] = 0;
-					for(uint32_t j = 0; j < 6; j++)
-					{
-						B[i + 0] += A[i + 6 * j] / 2 * dv[j + 0];
-						B[i + 6] += A[i + 6 * j] / 2 * dv[j + 6];
-					}
-				}
+				//gradient r
+				B[2 + 0] = 0;
+				B[4 + 0] = +(1 + s);
+				B[5 + 0] = -(1 + s);
+				B[1 + 0] = +(1 + 2 * r) / 2;
+				B[3 + 0] = -(1 + 2 * r + s);
+				B[0 + 0] = +(1 + 2 * r + 2 * s) / 2;
+				//gradient s
+				B[1 + 6] = 0;
+				B[3 + 6] = -(1 + r);
+				B[4 + 6] = +(1 + r);
+				B[2 + 6] = +(1 + 2 * s) / 2;
+				B[5 + 6] = -(1 + r + 2 * s);
+				B[0 + 6] = +(1 + 2 * r + 2 * s) / 2;
 			}
 		}
 	}
